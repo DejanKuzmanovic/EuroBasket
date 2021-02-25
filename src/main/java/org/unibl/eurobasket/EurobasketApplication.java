66efd15dcdf0dc9a6377faa1728877a7a874a5d9
Dejan Kuzmanovic <dejan.kuzmanovic@yahoo.com>
2021-02-25 20:19:47 +0100

    Generate Spring Boot project

---
+++ src/main/java/org/unibl/eurobasket/EurobasketApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.eurobasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurobasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurobasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/unibl/eurobasket/EurobasketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.eurobasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurobasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
